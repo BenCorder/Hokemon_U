2f474c9f133e13ca85dc7adeeca794f4f43470a5
Ben Corder <COR202002212@longroad.ac.uk>
2021-01-28 11:36:56 +0000

    halor, hinstinct classes created

---
+++ Hokemon_U/Halor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon_U
+{
+    class Halor : Hokemon // INHERITANCE we put : Hokemon to state where the parent is 
+    {
+
+
+
+    }
+}
---
+++ Hokemon_U/Hinstinct.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon_U
+{
+    class Hinstinct : Hokemon
+    {
+    }
+}
